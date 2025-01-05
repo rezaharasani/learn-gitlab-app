e9c7e57c8ed189452d243478d4875da2f8877114
Valentin Despa <valentin@vdespa.com>
2025-01-05 16:44:59 +0100

    Update linter config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['build'] },
+  { ignores: ['build', 'reports'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {

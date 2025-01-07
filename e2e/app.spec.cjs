c3f04f360b6df5b7bf39256d981618e3a21ff74e
Valentin Despa <valentin.despa@gmail.com>
2025-01-07 08:46:16 +0000

    Use VITE_APP_VERSION

--- e2e/app.spec.cjs
+++ e2e/app.spec.cjs
@@ -18,7 +18,7 @@ test('has GitLab in the body', async ({ page }) => {
 test('has expected app version', async ({ page }) => {
   await page.goto('/');
 
-  const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
+  const expectedAppVersion = process.env.VITE_APP_VERSION ? process.env.VITE_APP_VERSION : '1';
   const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
   expect(isVisible).toBeTruthy();
 });
